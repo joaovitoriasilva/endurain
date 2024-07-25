33838c9adb5cede8610deb69be35e67d9a4085db
João Vitória Silva <8648976+joaovitoriasilva@users.noreply.github.com>
2024-07-25 08:10:39 +0100

    Added Vite PWA for PWA support
    [frontend] added basic Vite PWA configuration for PWA support
    [docker-compose] fixed typo

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -3,10 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
   resolve: {
     alias: {
