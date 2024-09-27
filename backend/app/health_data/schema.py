af664c34d4d03d4b36baeb2a1d3fe49f7e53b938
João Vitória Silva <8648976+joaovitoriasilva@users.noreply.github.com>
2024-09-27 14:13:18 -0700

    Update schema.py
    [backend] fIxed issue with variable definition type

--- backend/app/health_data/schema.py
+++ backend/app/health_data/schema.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
-
+from datetime import date
 
 class HealthData(BaseModel):
     id: int | None = None
     user_id: int | None = None
-    created_at: str | None = None
+    created_at: date | None = None
     weight: float | None = None
 
     class Config:
