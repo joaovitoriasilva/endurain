5f065e941906337efd387b84f77f479a00a98498
João Vitória Silva <8648976+joaovitoriasilva@users.noreply.github.com>
2024-11-24 14:42:11 +0000

    Update authStore.js

--- frontend/app/src/stores/authStore.js
+++ frontend/app/src/stores/authStore.js
@@ -77,7 +77,7 @@ export const useAuthStore = defineStore('auth', {
         },
         setUserWebsocket() {
             console.log("Protocol is:", import.meta.env.VITE_BACKEND_PROTOCOL);
-            const protocol = import.meta.env.VITE_BACKEND_PROTOCOL === 'https' ? 'wss' : 'ws';
+            const protocol = import.meta.env.VITE_BACKEND_PROTOCOL === 'http' ? 'ws' : 'wss';
             const websocketURL = `${protocol}://${BACKEND_URL}ws/${this.user.id}`;
             console.log("WebSocket URL:", websocketURL);
             this.user_websocket = new WebSocket(websocketURL);
