0e43bf1a0946607814c044bf03ddd5eb12810788
João Vitória Silva <8648976+joaovitoriasilva@users.noreply.github.com>
2024-12-23 22:30:56 +0000

    Added proxy headers flag to FastAPI launch logic
    [docker] add new BEHIND_PROXY env variable with false as default value
    [docker] added new script start.sh with frontend.sh logic plus logic to add proxy headers flag if BEHIND_PROXY is true
    [docker] removed frontend.sh
    [docker] created new folder docker in root and moved docker related files to it

--- docker/start.sh
+++ docker/start.sh
@@ -1,11 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
 # Substitute MY_APP_ENDURAIN_HOST with the value of ENDURAIN_HOST
 if [ ! -z "$ENDURAIN_HOST" ]; then
+    echo "Substituting MY_APP_ENDURAIN_HOST with $ENDURAIN_HOST"
     find /app/backend/frontend/dist -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|MY_APP_ENDURAIN_HOST|${ENDURAIN_HOST}|g" '{}' +
 fi
 
 # Substitute MY_APP_STRAVA_CLIENT_ID with the value of STRAVA_CLIENT_ID
 if [ ! -z "$STRAVA_CLIENT_ID" ]; then
+    echo "Substituting MY_APP_STRAVA_CLIENT_ID with $STRAVA_CLIENT_ID"
     find /app/backend/frontend/dist -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|MY_APP_STRAVA_CLIENT_ID|${STRAVA_CLIENT_ID}|g" '{}' +
 fi
+
+echo "Starting FastAPI with BEHIND_PROXY=$BEHIND_PROXY"
+
+# Base command as an array
+CMD=("uvicorn" "main:app" "--host" "0.0.0.0" "--port" "80")
+
+# Add --proxy-headers if BEHIND_PROXY is true
+if [ "$BEHIND_PROXY" = "true" ]; then
+    echo "Enabling proxy headers"
+    CMD+=("--proxy-headers")
+fi
+
+# Execute the command
+exec "${CMD[@]}"
