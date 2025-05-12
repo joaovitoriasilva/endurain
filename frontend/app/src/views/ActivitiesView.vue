f7fd5fa3a2264ec77fb7f6fd862627e10c970d2b
João Vitória Silva <8648976+joaovitoriasilva@users.noreply.github.com>
2025-05-12 10:52:29 +0100

    Fix filter reset & colors, auto-search by name, optimize code
    [frontend] fixed clear filters was not resetting column sorting
    [frontend] fixed UI colors to be inline with rest of the code
    [frontend] searching by name automatically searches, no longer needed to click apply
    [frontend] code optimization

--- frontend/app/src/views/ActivitiesView.vue
+++ frontend/app/src/views/ActivitiesView.vue
@@ -41,7 +41,11 @@
 				<!-- Buttons -->
 				<div class="col-12 mt-3 d-flex justify-content-end gap-2">
 					<button type="button" class="btn btn-secondary" @click="clearFilters">{{ $t('activitiesView.buttonClear') }}</button>
-					<button type="submit" class="btn btn-primary">{{ $t('activitiesView.buttonApply') }}</button>
+					<button type="submit" class="btn btn-primary" disabled v-if="isLoading">
+						<span class="spinner-border spinner-border-sm me-1" aria-hidden="true"></span>
+						<span role="status">{{ $t('activitiesView.buttonApply') }}</span>
+					</button>
+					<button type="submit" class="btn btn-primary" v-else>{{ $t('activitiesView.buttonApply') }}</button>
 				</div>
 			</form>
 		</div>
@@ -50,7 +54,7 @@
 
 	<LoadingComponent v-if="isLoading" />
 
-	<div v-else>
+	<div class="p-3 bg-body-tertiary rounded shadow-sm" v-else-if="activities && activities.length">
 		<!-- Activities Table -->
 		<ActivitiesTableComponent
 			:activities="activities"
@@ -61,14 +65,16 @@
 		/>
 
 		<PaginationComponent :totalPages="totalPages" :pageNumber="pageNumber" @pageNumberChanged="setPageNumber" v-if="activities && activities.length"/>
-		
-		<NoItemsFoundComponents v-else />
 	</div>
+		
+	<NoItemsFoundComponents v-else />
 </template>
 
 <script>
 import { ref, onMounted, watch } from "vue";
 import { useI18n } from "vue-i18n";
+// import lodash
+import { debounce } from "lodash";
 // Import Notivue push
 import { push } from "notivue";
 // Import stores
@@ -90,18 +96,16 @@ export default {
 	},
 	setup() {
 		const { t } = useI18n();
-
 		const authStore = useAuthStore();
+		const activityTypes = ref([]);
 		const activities = ref([]);
 		const userNumberActivities = ref(0);
 		const pageNumber = ref(1);
 		const numRecords = 5;
 		const totalPages = ref(1);
 		const isLoading = ref(true);
-		const error = ref(null);
 
 		// Filter state
-		const activityTypes = ref([]);
 		const selectedType = ref("");
 		const startDate = ref("");
 		const endDate = ref("");
@@ -111,12 +115,32 @@ export default {
 		const sortBy = ref("start_time"); // Default sort column
 		const sortOrder = ref("desc"); // Default sort order
 
+		const performNameSearch = debounce(async () => {
+			// If the search nickname is empty, reset the list to initial state.
+			if (!nameSearch.value) {
+				// Reset the list to the initial state when search text is cleared
+				pageNumber.value = 1;
+				// Apply filters
+				await applyFilters();
+				// Exit the function
+				return;
+			}
+			try {
+				// Fetch the activities based on the search name.
+				await applyFilters();
+			} catch (error) {
+				push.error(`${t("activitiesView.errorUpdatingActivities")} - ${error}`);
+			}
+		}, 500);
+
 		// Fetch available activity types for the filter dropdown
 		async function fetchActivityTypes() {
 			try {
-				activityTypes.value = await activitiesService.getActivityTypes(); // Assuming a new service function
+				activityTypes.value = await activitiesService.getActivityTypes();
 			} catch (error) {
-				push.error(`${t("activitiesView.errorFailedFetchActivityTypes")} - ${error}`);
+				push.error(
+					`${t("activitiesView.errorFailedFetchActivityTypes")} - ${error}`,
+				);
 			}
 		}
 
@@ -131,7 +155,7 @@ export default {
 				isLoading.value = true;
 
 				// Fetch the gears with pagination.
-				fetchActivities({}, sortBy.value, sortOrder.value);
+				await fetchActivities();
 			} catch (error) {
 				// If there is an error, set the error message and show the error alert.
 				push.error(`${t("activitiesView.errorUpdatingActivities")} - ${error}`);
@@ -142,35 +166,25 @@ export default {
 		}
 
 		// Fetch activities with pagination, filters, and sorting
-		async function fetchActivities(
-			currentFilters = {},
-			currentSortBy = sortBy.value,
-			currentSortOrder = sortOrder.value,
-		) {
-			isLoading.value = true;
-			error.value = null;
+		async function fetchActivities() {
 			activities.value = [];
+			const filters = {
+				type: selectedType.value,
+				start_date: startDate.value,
+				end_date: endDate.value,
+				name_search: nameSearch.value,
+			};
 
 			try {
-				// Prepare filters, removing empty values
-				const activeFilters = {};
-				if (currentFilters.type) activeFilters.type = currentFilters.type;
-				if (currentFilters.start_date)
-					activeFilters.start_date = currentFilters.start_date;
-				if (currentFilters.end_date)
-					activeFilters.end_date = currentFilters.end_date;
-				if (currentFilters.name_search)
-					activeFilters.name_search = currentFilters.name_search;
-
 				// Use the activities service with filters and sorting
 				activities.value =
 					await activitiesService.getUserActivitiesWithPagination(
 						authStore.user.id,
 						pageNumber.value,
 						numRecords,
-						activeFilters,
-						currentSortBy, // Pass sort column
-						currentSortOrder, // Pass sort order
+						filters,
+						sortBy.value,
+						sortOrder.value,
 					);
 				userNumberActivities.value =
 					await activitiesService.getUserNumberOfActivities();
@@ -178,46 +192,37 @@ export default {
 				totalPages.value = Math.ceil(userNumberActivities.value / numRecords);
 			} catch (error) {
 				push.error(`${t("activitiesView.errorFetchingActivities")} - ${error}`);
-			} finally {
-				isLoading.value = false;
 			}
 		}
 
-		onMounted(() => {
-			fetchActivityTypes(); // Fetch types on mount
-			// Initial fetch with default page, filters (empty), and sorting
-			fetchActivities({}, sortBy.value, sortOrder.value);
+		onMounted(async () => {
+			// fetch initial data
+			await fetchActivityTypes();
+			await updateActivities();
 		});
 
 		// Function to apply filters and fetch data
-		function applyFilters() {
+		async function applyFilters() {
 			// Reset to page 1 when filters change
 			pageNumber.value = 1;
 			// keep current sort order
-			fetchActivities(
-				{
-					type: selectedType.value,
-					start_date: startDate.value,
-					end_date: endDate.value,
-					name_search: nameSearch.value,
-				},
-				sortBy.value,
-				sortOrder.value,
-			);
+			await updateActivities();
 		}
 
 		// Function to clear filters
-		function clearFilters() {
+		async function clearFilters() {
 			selectedType.value = "";
 			startDate.value = "";
 			endDate.value = "";
 			nameSearch.value = "";
+			sortBy.value = "start_time";
+			sortOrder.value = "desc";
 			// applyFilters will re-fetch with cleared filters and current sort order
-			applyFilters();
+			await applyFilters();
 		}
 
 		// Function to handle sorting changes from table component
-		function handleSort(columnName) {
+		async function handleSort(columnName) {
 			if (sortBy.value === columnName) {
 				// Toggle sort order if same column is clicked
 				sortOrder.value = sortOrder.value === "asc" ? "desc" : "asc";
@@ -227,17 +232,11 @@ export default {
 				sortOrder.value = "desc"; // Or 'asc' if preferred as default
 			}
 			// Fetch data with new sorting, reset to page 1
-			fetchActivities(
-				{
-					type: selectedType.value,
-					start_date: startDate.value,
-					end_date: endDate.value,
-					name_search: nameSearch.value,
-				},
-				sortBy.value,
-				sortOrder.value,
-			);
+			await updateActivities();
 		}
+
+		// Watch the search name variable.
+		watch(nameSearch, performNameSearch, { immediate: false });
 
 		// Watch the page number variable.
 		watch(pageNumber, updateActivities, { immediate: false });
@@ -245,10 +244,8 @@ export default {
 		return {
 			activities,
 			pageNumber,
-			numRecords,
 			totalPages,
 			isLoading,
-			error,
 			activityTypes,
 			selectedType,
 			startDate,
@@ -256,7 +253,6 @@ export default {
 			nameSearch,
 			sortBy,
 			sortOrder,
-			fetchActivities,
 			setPageNumber,
 			applyFilters,
 			clearFilters,
