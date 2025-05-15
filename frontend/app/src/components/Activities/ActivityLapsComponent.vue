06b12ccc8a65787e539cd07f3f38548d115c77ca
rob <robert.g.wakefield@gmail.com>
2025-05-15 23:30:35 +0100

    Update laps table to show rests in different bg color

--- frontend/app/src/components/Activities/ActivityLapsComponent.vue
+++ frontend/app/src/components/Activities/ActivityLapsComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="table-responsive d-none d-sm-block">
-        <table class="table table-striped table-borderless table-hover table-sm rounded text-center" style="--bs-table-bg: var(--bs-gray-850);">
+        <table class="table table-borderless table-hover table-sm rounded text-center" :class="{ 'table-striped': activity.activity_type !== 8 && activity.activity_type !== 9 }" style="--bs-table-bg: var(--bs-gray-850);">
             <thead>
                 <tr>
                     <th>{{ $t("activityLapsComponent.labelLapNumber") }}</th>
-                    <th>{{ $t("activityLapsComponent.labelLapIntensity") }}</th>
+                    <th v-if="hasIntensity">{{ $t("activityLapsComponent.labelLapIntensity") }}</th>
                     <th>{{ $t("activityLapsComponent.labelLapDistance") }}</th>
                     <th>{{ $t("activityLapsComponent.labelLapTime") }}</th>
                     <th v-if="activity.activity_type === 4 || activity.activity_type === 5 || activity.activity_type === 6 || activity.activity_type === 7 || activity.activity_type === 27">{{ $t("activityLapsComponent.labelLapSpeed") }}</th>
@@ -17,9 +17,9 @@
                 </tr>
             </thead>
             <tbody class="table-group-divider">
-                <tr v-for="(lap, index) in normalizedLaps" :key="lap.id">
+                <tr v-for="(lap, index) in normalizedLaps" :key="lap.id" :style="{ 'background-color': !lap.swimIsRest ? 'var(--bs-table-striped-bg)' : 'var(--bs-table-bg)' }">
                     <td>{{ index + 1 }}</td>
-                    <td>{{ lap.intensity ?? $t("generalItems.labelNoData") }}</td>
+                    <td v-if="hasIntensity">{{ lap.intensity ?? $t("generalItems.labelNoData") }}</td>
                     <td>{{ lap.formattedDistance }}</td>
                     <td>{{ lap.lapSecondsToMinutes }}</td>
                     <td v-if="activity.activity_type === 4 || activity.activity_type === 5 || activity.activity_type === 6 || activity.activity_type === 7 || activity.activity_type === 27">{{ lap.formattedSpeedFull }}</td>
@@ -136,7 +136,7 @@ export default {
 			const fastestPace = Math.min(...enhancedAvgPaces);
 
 			// Normalize each lap's pace relative to the fastest
-			return laps.map((lap) => {
+			const normalizedLaps = laps.map((lap, index) => {
 				const normalizedScore = (fastestPace / lap.enhanced_avg_pace) * 100;
 				const formattedPace = computed(() => {
 					if (
@@ -220,6 +220,9 @@ export default {
                     }
                     return formatAverageSpeedImperial(lap.enhanced_avg_speed) ?? 0;
                 });
+                
+                const swimIsRest = (props.activity.activity_type === 8 || props.activity.activity_type === 9) 
+                    && (lap.total_distance === 0 || lap.total_distance === null);
 
 				return {
 					...lap,
@@ -232,12 +235,27 @@ export default {
                     formattedElevationFull: formattedElevationFull,
                     formattedSpeedFull: formattedSpeedFull,
                     formattedSpeed: formattedSpeed,
+                    swimIsRest: swimIsRest,
 				};
 			});
+            
+            // Assume that 2 rests in a row is an error/drills
+            for (let i = 0; i < normalizedLaps.length - 1; i++) {
+                if (normalizedLaps[i].swimIsRest === true && normalizedLaps[i + 1].swimIsRest === true) {
+                    normalizedLaps[i + 1].swimIsRest = false;
+                }
+            }
+
+            return normalizedLaps;
 		});
+        
+        const hasIntensity = computed(() => {
+            return normalizedLaps.value.some(lap => lap.intensity !== null);
+        });
 
 		return {
 			normalizedLaps,
+            hasIntensity,
 		};
 	},
 };
