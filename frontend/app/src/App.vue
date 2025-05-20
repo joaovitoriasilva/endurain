a9815472888113c0e89fffb93c2b93a6cdcab987
João Vitória Silva <8648976+joaovitoriasilva@users.noreply.github.com>
2025-05-20 11:44:52 +0100

    Attempt fix for PWA

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -10,37 +10,44 @@ import {
 	NotificationProgress,
 	pastelTheme,
 } from "notivue";
-import { UseScreenSafeArea } from "@vueuse/components";
+import { useScreenSafeArea } from '@vueuse/core'
+
+const {
+  top,
+  right,
+  bottom,
+  left,
+} = useScreenSafeArea()
 </script>
 
 <template>
-	<UseScreenSafeArea v-slot="{ top, right, bottom, left }">
-		<Notivue v-slot="item">
-			<NotivueSwipe :item="item">
-				<Notification :item="item" :theme="pastelTheme">
-					<NotificationProgress :item="item" />
-				</Notification>
-			</NotivueSwipe>
-		</Notivue>
-		<div class="d-flex flex-column vh-100">
-			<!-- Top Navbar with safe-area padding -->
-			<div class="bg-body-tertiary shadow-sm" :style="{ paddingTop: `${top}px` }">
-				<NavbarComponent class="container"/>
-			</div>
-
-			<!-- Main content -->
-			<main class="container py-4 flex-grow-1">
-				<RouterView />
-			</main>
-
-			<!-- Desktop Footer -->
-			<FooterComponent class="d-none d-lg-block shadow-sm"/>
-
-			<!-- Bottom Mobile Navbar with safe-area padding -->
-			<NavbarBottomMobileComponent 
-				class="d-lg-none d-block sticky-bottom shadow-sm"
-				:style="{ paddingBottom: `${bottom}px` }"
-			/>
+	{{ top }} {{ right }} {{ bottom }} {{ left }}
+	top: {{ top }}px
+	<Notivue v-slot="item">
+		<NotivueSwipe :item="item">
+			<Notification :item="item" :theme="pastelTheme">
+				<NotificationProgress :item="item" />
+			</Notification>
+		</NotivueSwipe>
+	</Notivue>
+	<div class="d-flex flex-column vh-100">
+		<!-- Top Navbar with safe-area padding -->
+		<div class="bg-body-tertiary shadow-sm" :style="{ paddingTop: `${top}px` }">
+			<NavbarComponent class="container"/>
 		</div>
-	</UseScreenSafeArea>
+
+		<!-- Main content -->
+		<main class="container py-4 flex-grow-1">
+			<RouterView />
+		</main>
+
+		<!-- Desktop Footer -->
+		<FooterComponent class="d-none d-lg-block shadow-sm"/>
+
+		<!-- Bottom Mobile Navbar with safe-area padding -->
+		<NavbarBottomMobileComponent 
+			class="d-lg-none d-block sticky-bottom shadow-sm"
+			:style="{ paddingBottom: `${bottom}px` }"
+		/>
+	</div>
 </template>
