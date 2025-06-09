99677aa236551b9f721abdf8cbe804164fde995d
simplicityf <omobolanlehazeezat@gmail.com>
2025-06-10 00:50:33 +0100

    fix: resolve codes issues
    chore: resolve missing dependecies and unsed dependecy

--- frontend/app/src/services/profileService.js
+++ frontend/app/src/services/profileService.js
@@ -1,31 +1,43 @@
-import { fetchGetRequest, fetchPostRequest, fetchPutRequest, fetchDeleteRequest, fetchPostFileRequest } from '@/utils/serviceUtils';
+import {
+  fetchGetRequest,
+  fetchPostRequest,
+  fetchPutRequest,
+  fetchDeleteRequest,
+  fetchPostFileRequest
+} from '@/utils/serviceUtils'
 
 export const profile = {
-    getProfileInfo() {
-        return fetchGetRequest('profile');
-    },
-    getProfileSessions() {
-        return fetchGetRequest('profile/sessions');
-    },
-    uploadProfileImage(file) {
-        const formData = new FormData();
-        formData.append('file', file);
+  getProfileInfo() {
+    return fetchGetRequest('profile')
+  },
+  getProfileSessions() {
+    return fetchGetRequest('profile/sessions')
+  },
+  uploadProfileImage(file) {
+    const formData = new FormData()
+    formData.append('file', file)
 
-        return fetchPostFileRequest('profile/image', formData);
-    },
-    editProfile(data) {
-        return fetchPutRequest('profile', data)
-    },
-    editUserPrivacySettings(data) {
-        return fetchPutRequest('profile/privacy', data);
-    },
-    editProfilePassword(data) {
-        return fetchPutRequest('profile/password', data)
-    },
-    deleteProfilePhoto() {
-        return fetchPutRequest('profile/photo');
-    },
-    deleteProfileSession(session_id) {
-        return fetchDeleteRequest(`profile/sessions/${session_id}`);
-    }
-};
+    return fetchPostFileRequest('profile/image', formData)
+  },
+  editProfile(data) {
+    return fetchPutRequest('profile', data)
+  },
+  editUserPrivacySettings(data) {
+    return fetchPutRequest('profile/privacy', data)
+  },
+  editProfilePassword(data) {
+    return fetchPutRequest('profile/password', data)
+  },
+  deleteProfilePhoto() {
+    return fetchPutRequest('profile/photo')
+  },
+  deleteProfileSession(session_id) {
+    return fetchDeleteRequest(`profile/sessions/${session_id}`)
+  },
+  exportData() {
+    return fetchGetRequest('profile/export', { responseType: 'blob' })
+  },
+  importData(formData) {
+    return fetchPostFileRequest('profile/import', formData)
+  }
+}
