1c1c8e53169b194c970db00773887367ab431002
João Vitória Silva <8648976+joaovitoriasilva@users.noreply.github.com>
2025-06-17 16:51:26 +0100

    Added cassette, chain and left crank PM

--- frontend/app/src/utils/gearComponentsUtils.js
+++ frontend/app/src/utils/gearComponentsUtils.js
@@ -3,6 +3,8 @@ export function getGearComponentType(type, t) {
 	const avatarMap = {
 		"back_tire": t("gearComponentListComponent.gearComponentBackTire"),
 		"front_tire": t("gearComponentListComponent.gearComponentFrontTire"),
+		"cassette": t("gearComponentListComponent.gearComponentCassette"),
+		"chain": t("gearComponentListComponent.gearComponentChain"),
 	};
 
 	return avatarMap[type];
@@ -12,6 +14,8 @@ export function getGearComponentAvatar(type) {
 	const avatarMap = {
 		"back_tire": "/src/assets/avatar/gearComponents/tire1.png",
 		"front_tire": "/src/assets/avatar/gearComponents/tire1.png",
+		"cassette": "/src/assets/avatar/gearComponents/cassette1.png",
+		"chain": "/src/assets/avatar/gearComponents/chain1.png",
 	};
 
 	return avatarMap[type];
