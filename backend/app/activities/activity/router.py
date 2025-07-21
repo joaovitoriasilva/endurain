4c7b51d5a9e854d90e2fd3de40feb2b7e43eb8f2
Marc @PDev1 server <mp@polkunet.vip>
2025-07-21 10:10:29 +0000

    More log file comment clarification.

--- backend/app/activities/activity/router.py
+++ backend/app/activities/activity/router.py
@@ -565,6 +565,8 @@ async def create_activity_with_bulk_import(
     background_tasks: BackgroundTasks,
 ):
     try:
+        core_logger.print_to_log_and_console("Bulk import initiated.")
+
         # Ensure the 'bulk_import' directory exists
         bulk_import_dir = core_config.FILES_BULK_IMPORT_DIR
         os.makedirs(bulk_import_dir, exist_ok=True)
@@ -585,7 +587,7 @@ async def create_activity_with_bulk_import(
                 )
 
         # Return a success message
-        return {"Bulk import initiated. Processing files in the background."}
+        return {"Bulk import initiated for all files found in bulk_import directory. Processing files will continue in the background."}
     except Exception as err:
         # Log the exception
         core_logger.print_to_log(
