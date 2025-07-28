3273b875d721115ba991b5d481250d109f0ed319
Marc @PDev1 server <mp@polkunet.vip>
2025-07-28 20:27:19 +0000

    adding strava bike import call to frontend gears service.

--- frontend/app/src/services/gearsService.js
+++ frontend/app/src/services/gearsService.js
@@ -30,5 +30,8 @@ export const gears = {
     },
     deleteGear(gearId) {
         return fetchDeleteRequest(`gears/${gearId}`);
+    },
+    stravaBikesImport() {
+        return fetchPostRequest('gears/stravabikesimport');
     }
 };
