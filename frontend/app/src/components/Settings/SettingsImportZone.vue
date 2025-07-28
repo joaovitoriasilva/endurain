2327a2030b56f175d05c1d3cdc259a810fefb339
Marc @PDev1 server <mp@polkunet.vip>
2025-07-28 20:17:56 +0000

    Creating an import zone for the front end.

---
+++ frontend/app/src/components/Settings/SettingsImportZone.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="col">
+		<div class="bg-body-tertiary rounded p-3 shadow-sm">
+			<!-- list zone -->
+			<ul class="list-group list-group-flush">
+				<!-- import zone -->
+				<!-- bulk import zone -->
+				<li class="list-group-item d-flex justify-content-between bg-body-tertiary px-0">
+					<div class="d-flex align-items-center">
+						<font-awesome-icon :icon="['fas', 'file-import']" size="2x" />
+						<div class="ms-3">
+							<div class="fw-bold">
+								{{ $t("settingsImportZone.bulkImportIntegrationTitle") }}
+							</div>
+							{{ $t("settingsImportZone.bulkImportIntegrationBody") }}
+						</div>
+					</div>
+					<div class="d-flex align-items-center">
+						<!-- import button -->
+						<a href="#" class="btn btn-primary" role="button" @click="submitBulkImport">{{ $t("settingsImportZone.buttonBulkImport") }}</a>
+					</div>
+				</li>
+				<!-- Strava bulk-export import zone -->
+				<li class="list-group-item d-flex justify-content-between bg-body-tertiary px-0">
+					<div class="d-flex align-items-center">
+						<font-awesome-icon :icon="['fas', 'file-import']" size="2x" />
+						<div class="ms-3">
+							<div class="fw-bold">
+								{{ $t("settingsImportZone.stravaImportIntegrationTitle") }}
+							</div>
+							{{ $t("settingsImportZone.stravaImportIntegrationBody") }}
+						</div>
+					</div>
+					<div class="d-flex align-items-center">
+						<!-- import button -->
+						<a href="#" class="btn btn-primary" role="button" @click="submitStravaBikesImport">{{ $t("settingsImportZone.stravaImportbuttonBikes") }}</a>
+					</div>
+				</li>
+			</ul>
+
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+// Import Notivue push
+import { push } from "notivue";
+// Importing the stores
+import { useAuthStore } from "@/stores/authStore";
+// Importing the services
+import { activities } from "@/services/activitiesService";
+import { gears } from "@/services/gearsService";
+// Import the components
+import ModalComponent from "@/components/Modals/ModalComponent.vue";
+import ModalComponentNumberAndStringInput from "@/components/Modals/ModalComponentNumberAndStringInput.vue";
+import ModalComponentNumberInput from "@/components/Modals/ModalComponentNumberInput.vue";
+import ModalComponentDateRangeInput from "@/components/Modals/ModalComponentDateRangeInput.vue";
+export default {
+	components: {
+		ModalComponent,
+		ModalComponentNumberAndStringInput,
+		ModalComponentNumberInput,
+		ModalComponentDateRangeInput,
+		//GarminConnectLoginModalComponent,
+	},
+	setup() {
+		const authStore = useAuthStore();
+		const { locale, t } = useI18n();
+		async function submitBulkImport() {
+			try {
+				await activities.bulkImportActivities();
+				// Show the loading alert.
+				push.info(t("settingsImportZone.loadingMessageBulkImport"));
+			} catch (error) {
+				// If there is an error, show the error alert.
+				push.error(
+					`${t("settingsImportZone.errorMessageUnableToImportActivities")} - ${error}`,
+				);
+			}
+		}
+		async function submitStravaBikesImport() {
+			try {
+				await gears.stravaBikesImport();
+				// Show the loading alert.
+				push.info(t("settingsImportZone.loadingMessageStravaBikesImport"));
+			} catch (error) {
+				// If there is an error, show the error alert.
+				push.error(
+					`${t("settingsImportZone.errorMessageUnableToImportBikes")} - ${error}`,
+				);
+			}
+		}
+		return {
+			authStore,
+			t,
+			submitBulkImport,
+			submitStravaBikesImport,
+		};
+	},
+};
+</script>
