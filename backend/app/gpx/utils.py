9ddf53f90405e7a964446f6e5e51baa29735a95d
Boris Stäheli <ich@bstaehe.li>
2025-10-15 11:56:57 +0200

    Add support for heart rate extraction from GPX files from Tissot Smart Watches

--- backend/app/gpx/utils.py
+++ backend/app/gpx/utils.py
@@ -160,6 +160,9 @@ def parse_gpx_file(
                                         elif extension.tag.endswith("power"):
                                             # Extract 'power' value
                                             power = extension.text
+                                        elif extension.tag.endswith("heartrate"):
+                                            # Tissot T-Touch smartwatch and similar devices extension
+                                            heart_rate = extension.text
 
                                 # Check if heart rate, cadence, power are set
                                 if heart_rate != 0:
